bffb2a9181a5c258c17511232e26dba70903e560
Louie Sebastian Kurenai <168345503+ruikurenaii@users.noreply.github.com>
2025-07-13 15:14:56 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// last update: check the website
+const android_rank = 18;
+const ios_rank = 829;
+
+// strings
+const android_string = `Rank #${android_rank}`;
+const ios_string = `Rank #${ios_rank}`;
+
+// scripts
+document.getElementById("android-data").textContent = `${android_string}`;
+document.getElementById("ios-data").textContent = `${ios_string}`;
