3bb2f49a68b9890090ec07150701f4ce831e3759
Louie Sebastian Kurenai <168345503+ruikurenaii@users.noreply.github.com>
2025-07-15 05:27:41 +0800

    update rank

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // last update: check the website
-const android_rank = 18;
-const ios_rank = 768;
+const android_rank = 17;
+const ios_rank = 713;
 
 // strings
 const android_string = `Rank #${android_rank}`;
